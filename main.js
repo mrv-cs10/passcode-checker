8c953eea33fe15becefb38cc460a2593ccd0a78d
mrveldkamp <37160046+mrveldkamp@users.noreply.github.com>
2023-02-11 16:03:47 -0700

    start-code

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-// Program Title
+// Passcode Checker
+
+// Button Event Listener
+document.getElementById("btn").addEventListener("click", btnClicked);
+
+function btnClicked() {
+  // Inputs
+  let code1 = document.getElementById("code1-in").value;
+  let code2 = document.getElementById("code2-in").value;
+  let code3 = document.getElementById("code3-in").value;
+
+  // Check Passcode
+}
