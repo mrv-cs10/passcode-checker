1b38b4db0bfc13ee1fb641f405df7bde723cf198
mrveldkamp <37160046+mrveldkamp@users.noreply.github.com>
2023-02-11 16:20:44 -0700

    update to number input

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ document.getElementById("btn").addEventListener("click", btnClicked);
 
 function btnClicked() {
   // Inputs
-  let code1 = document.getElementById("code1-in").value;
-  let code2 = document.getElementById("code2-in").value;
-  let code3 = document.getElementById("code3-in").value;
+  let code1 = +document.getElementById("code1-in").value;
+  let code2 = +document.getElementById("code2-in").value;
+  let code3 = +document.getElementById("code3-in").value;
 
   // Check Passcode
 }
